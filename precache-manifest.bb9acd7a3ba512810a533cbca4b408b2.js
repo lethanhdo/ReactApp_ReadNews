e722cf20a01d44f48ac92ee3cd9a53e45fb05d3f
CoolerMan <lethanhdo.github.com>
2020-06-09 10:45:47 +0700

    Updates

---
+++ precache-manifest.bb9acd7a3ba512810a533cbca4b408b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa901ce750acd3bd51bd8126a528930",
+    "url": "/ReadNews_React/index.html"
+  },
+  {
+    "revision": "1f3abcce6b7ec5fe212c",
+    "url": "/ReadNews_React/static/css/main.b688d1ae.chunk.css"
+  },
+  {
+    "revision": "7077c7d3d421155287d2",
+    "url": "/ReadNews_React/static/js/2.c6b189a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReadNews_React/static/js/2.c6b189a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3abcce6b7ec5fe212c",
+    "url": "/ReadNews_React/static/js/main.71d33d5a.chunk.js"
+  },
+  {
+    "revision": "3e126b25a1944e8631c4",
+    "url": "/ReadNews_React/static/js/runtime-main.d81a79c3.js"
+  }
+]);
